114be44404534f53a58bac5ec2deadbb06311829
Andreas Olsson <andreas@actlea.com>
2021-12-05 22:01:42 +0100

    Changing printout to the new feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Change 6 and 7 combined");
+        System.out.println("Hello World! This is a new feature!");
     }
 }
