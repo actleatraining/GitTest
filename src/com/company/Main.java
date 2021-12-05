69c909595861f4f33e97377a0c17cca8cd809339
Andreas Olsson <andreas@actlea.com>
2021-12-05 15:28:14 +0100

    Changing printout in the clone

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Change 4");
+        System.out.println("Hello World! Change 5 - from Clone");
     }
 }
